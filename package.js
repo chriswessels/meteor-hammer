6be86bc6ee763bafcb9dd70ae33bb1a8ac827419
Chris Wessels <undefined.za@gmail.com>
2014-11-13 18:39:25 +0000

    Fixed inclusion specifier.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'chriswessels:hammer',
   summary: "A smart-package for integrating with Hammer.js for multi-touch gestures.",
-  version: "2.0.4",
+  version: "2.0.4_1",
   git: "https://github.com/chriswessels/meteor-hammer.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('lib/hammer.js');
+  api.addFiles('lib/hammer.js', 'client');
 });
 
