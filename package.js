615080ae6643a147eefd149f958b6d83b84b39f5
Chris Wessels <undefined.za@gmail.com>
2015-03-10 16:38:26 +0000

    3.0.0 stable release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'chriswessels:hammer',
   summary: "Bundles Hammer.js and provides Template.templateName.gestures() for easy use of multitouch gestures.",
-  version: "3.0.0-rc3",
+  version: "3.0.0",
   git: "https://github.com/chriswessels/meteor-hammer.git"
 });
 
